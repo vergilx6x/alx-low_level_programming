92e5cfffb0298ea0e5db21c36546ceca47a78d3c
Med Amine <med.work96@hotmail.com>
2023-08-14 11:01:37 -0700

    C

---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+   printf("Hello, World!\n");
+   char name[]="Amine";
+   int age=26;
+   printf("My name is %s\n",name);
+   printf("i'm %d old\n",age);
+   return 0;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+**0x0E. C - Structures, typedef**
+-This project is about learning about:
+-What are structures, when, why and how to use them
+-How to use typedef
