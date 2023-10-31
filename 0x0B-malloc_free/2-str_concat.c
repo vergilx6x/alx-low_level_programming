502958c93251cb3ec7564381820c654396d2416c
vergilx6x <med.work96@hotmail.com>
2023-10-31 16:32:24 -0700

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,14 +22,14 @@ char *str_concat(char *s1, char *s2)
   {
      i = 0;
      while (s1[i++] != '\0')
-        len1++;
+        len1++ ;
   }
 
   if (s2 != NULL)
   {
      i = 0;
      while (s2[i++] != '\0')
-        len2++;
+        len2++ ;
   }
 
   len = len1 + len2;
