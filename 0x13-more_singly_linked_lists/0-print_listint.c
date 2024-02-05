f714c1293fb83561b5235272f824952345d73ec5
vergilx6x <med.work96@hotmail.com>
2024-02-05 08:36:44 -0800

    header_readme_tesk1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to a linked list type(listint_t)
+ *
+ * return : @num: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
