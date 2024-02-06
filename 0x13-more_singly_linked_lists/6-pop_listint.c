6955466569f84cbbcecb7302f53aa0fa802effcd
vergilx6x <med.work96@hotmail.com>
2024-02-06 13:27:43 -0800

    fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,11 +4,11 @@
  * pop_listint - deletes the first node of the linked list
  * @head: pointer to the first node in the linked list
  *
- * Return: the data that was stored in the deleted node 
+ * Return: the data that was stored in the deleted node
  * or 0 if the list is empty
  */
 
-int pop_listint(lisint_t **head)
+int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int num;
