f795254ebc8d4f9aee5abd25a02f06e0dc35bbdc
vergilx6x <med.work96@hotmail.com>
2024-04-12 16:25:23 -0700

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,26 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: Null if an error occured
+ *         or a pointer to the new hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = (hash_table_t*)malloc(sizeof(hash_table_t));
+	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (!ht)
 	{
-		printf("Memory allocation failed\n");
-		exit(EXIT_FAILURE);
+		return (0);
 	}
 
-	ht->array = (hash_node_t**)calloc(size, sizeof(hash_node_t));
+	ht->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
 	if (ht->array == NULL)
 	{
-		printf("Memory allocation failed\n");
-		exit(EXIT_FAILURE);
+		return (0);
 	}
 
 	ht->size = size;
