d8493d751a6a40c500fc5b8490863f753511cc22
vergilx6x <med.work96@hotmail.com>
2024-07-17 12:44:54 -0700

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		else if (array[i] != value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		}
+	}
+
+	return (-1);
+}
